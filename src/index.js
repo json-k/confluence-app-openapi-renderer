9e892cfcb16029eed42bb733bf98e019a77755a1
Jase <jason@keeber.org>
2021-05-20 09:17:56 -0400

    Read file from URL

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import Resolver  from '@forge/resolver';
 import api from "@forge/api";
 import { fetch } from '@forge/api';
-import ForgeUI, { render, Fragment, Text, Macro, useProductContext, useConfig, MacroConfig, TextArea, Image, SectionMessage, isContentActionExtensionContext } from "@forge/ui";
+import ForgeUI, { render, Fragment, Text, Macro, useProductContext, useConfig, MacroConfig, TextArea, TextField,Image, SectionMessage, isContentActionExtensionContext } from "@forge/ui";
 
 const resolver = new Resolver();
 
 
 resolver.define('getConfig', ({ payload, context }) => {
   const cfg = context.extension.config
+  if(cfg && cfg.apiSrc){
+    return api.fetch(cfg.apiSrc).then(response => response.json())
+    .then(data => {
+      return data;
+    })
+  }
   if(cfg && cfg.apiData) {
     return  JSON.parse(cfg.apiData);
   }
@@ -20,6 +26,7 @@ const Config = () => {
   return (
     <MacroConfig>
       {/* Form components */}
+      <TextField name="apiSrc" label="Open API URL" />
       <TextArea name="apiData" label="OpenAPI JSON" />
     </MacroConfig>
   );
